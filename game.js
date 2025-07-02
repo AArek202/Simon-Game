52e935e19b59e44ab0b47349931ecd593422d2fe
AArek202 <abdelrahaman.amer202@gmail.com>
2025-07-02 15:20:11 +0300

    Add files to Simon Game

--- game.js
+++ game.js
@@ -19,16 +19,6 @@ $(document).keydown(function(){
 
 });
 
-$(document).click(function(){
-
-  if (!started) {
-    $("#level-title").text("Level " + level);
-    nextSequence();
-    started = true;
-  }
-
-});
-
 $(".btn").click(function(){
     var userChosenColour = $(this).attr("id");
     userClickedPattern.push(userChosenColour);
